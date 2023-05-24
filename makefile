4fb5957fc3f8dee9aa89e0b78e8ef6d2654c7df1
AlfieriChou <alfierichou@gmail.com>
2023-05-24 20:31:02 +0800

    chore: sync commodity monitor

--- makefile
+++ makefile
@@ -11,9 +11,17 @@ koala:
 	git push origin master
 
 commodity-monitor:
+	node scripts/dyFeAlloyMonitor.js
 	node scripts/neodymiumMonitor.js
+	node scripts/neodymiumOxideMonitor.js
 	node scripts/praseodymiumMonitor.js
 	node scripts/praseodymiumNeodymiumOxideMonitor.js
 	git add .
 	git commit -m 'chore: sync commodity monitor'
+	git push origin master
+
+unlock-stock:
+	node scripts/unlockStockList.js
+	git add .
+	git commit -m 'chore: sync unlock stock list'
 	git push origin master
---
+++ scripts/dyFeAlloyMonitor.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const path = require('path')
+const got = require('got')
+const { hash } = require('@galenjs/factories/crypto')
+
+const logger = console
+const url = 'http://www.100ppi.com/kx/'
+
+const getList = async (page) => {
+  const infoUrl = `${url}detail-message-984--${page}.html`
+  const infoRet = await got(infoUrl)
+  const list = infoRet.body
+    .split('\r\n')
+    .filter(item => item.includes('[镝铁合金]'))
+    .map(item => {
+      const [, content] = item.split('[镝铁合金]')
+      const [message, dateStrSpan] = content.split(' <span>')
+      const [dateStr, price] = message.split('镝铁合金为')
+      const date = dateStrSpan.split('</span>')[0]
+      return {
+        id: hash(`${dateStr}_镝铁合金`),
+        dateStr: `${new Date().getFullYear()}年${dateStr}`,
+        date: new Date(date).setHours(0, 0, 0, 0),
+        message,
+        name: '镝铁合金',
+        price: parseFloat(price)
+      }
+    })
+  return list
+}
+
+const start = async () => {
+  const list = [
+    ...await getList(1),
+    ...await getList(2),
+    ...await getList(3)
+  ]
+  let existList = []
+  const filePath = path.resolve(__dirname, '../json/dyFeAlloy.json')
+  if (fs.existsSync(filePath)) {
+    existList = require(filePath)
+    list.forEach(item => {
+      const exist = existList.find(i => i.id === item.id)
+      if (!exist) {
+        existList.push(item)
+      }
+    })
+  } else {
+    existList = list
+  }
+  logger.info('dyFeAlloy list', list.length)
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(existList.sort((a, b) => b.date - a.date), null, 2)
+  )
+  const mdList = []
+  mdList.push('# 镝铁合金价格变动趋势 \n')
+  mdList.push('| 时间 | 价格 | 消息正文 |')
+  mdList.push('|:--:|:--:|:--:|')
+  existList.forEach((item) => {
+    mdList.push(`|${item.dateStr}|${item.price}|${item.message}|`)
+  })
+  fs.writeFileSync('./markdown/dyFeAlloy.md', mdList.join('\n'))
+  logger.info('write file done')
+}
+
+start()
