50cf9423ec8a92259f4f6ffe963d9e765e4f8d4b
AlfieriChou <alfierichou@gmail.com>
2023-09-02 15:27:46 +0800

    chore: sync morgan stanley security hold list

--- scripts/morganStanleyItems.js
+++ scripts/morganStanleyItems.js
@@ -3,6 +3,7 @@ const path = require('path')
 const { sleep } = require('@galenjs/factories/sleep')
 const { camelJsonKeys } = require('@galenjs/factories/lodash')
 const { format } = require('date-fns')
+const ChartJsImage = require('chartjs-to-image')
 
 const Api = require('../lib/api')
 
@@ -12,6 +13,32 @@ const api = new Api({
   headers: {},
   logger
 })
+
+const generateLineChartImg = async ({
+  name, filename, xList, yList
+}) => {
+  const chart = new ChartJsImage()
+  chart
+    .setConfig({
+      type: 'line',
+      data: {
+        labels: xList,
+        datasets: [
+          {
+            fillColor: 'rgba(220,220,220,0.5)',
+            strokeColor: 'rgba(220,220,220,1)',
+            pointColor: 'rgba(220,220,220,1)',
+            pointStrokeColor: '#fff',
+            data: yList,
+            label: name
+          }
+        ]
+      }
+    })
+    .setWidth(3000)
+    .setHeight(1000)
+    .toFile(filename)
+}
 
 const getList = async ({
   page = 1,
@@ -52,12 +79,29 @@ const start = async (
   })
   const filePath = path.resolve(__dirname, `../json/morganStanley/${code}_data.json`)
   logger.info('morganStanley', code, list.length)
+  const chartList = list
+    .map(i => {
+      return {
+        ...i,
+        date: new Date(i.holdDate)
+      }
+    })
+    .sort((a, b) => a.date - b.date)
+  await generateLineChartImg({
+    name: code,
+    filename: path.join(process.cwd(), `img/morganStanley_${code}.png`),
+    xList: chartList.map(i => i.dateStr),
+    yList: chartList.map(i => i.holdNum)
+  })
   fs.writeFileSync(
     filePath,
     JSON.stringify(list, null, 2)
   )
   const mdList = []
   mdList.push(`# 摩根史丹利-${code}-持仓明细 \n`)
+  mdList.push('\n')
+  mdList.push(`![${code}](../../img/morganStanley_${code}.png)\n`)
+  mdList.push('\n')
   mdList.push('| 标的代码 | 标的名称 | 日期 | 持股数量 | A股占比 | 持股市值 |')
   mdList.push('|:--:|:--:|:--:|:--:|:--:|:--:|')
   list.forEach((item) => {
@@ -70,4 +114,5 @@ const start = async (
 ['002555', '002236'].reduce(async (promise, code) => {
   await promise
   await start(code)
+  await sleep(Math.floor(Math.random() * (20000 - 10000) + 10000))
 }, Promise.resolve())
